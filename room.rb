1ca5e569262e13a7a459e604d0ffa9e6519f7330
Can Toraman <cantoraman@gmail.com>
2018-06-01 11:51:22 +0100

    room guest taker works in normal conditions

--- room.rb
+++ room.rb
@@ -1,16 +1,25 @@
 class Room
 
   attr_reader :id, :till, :capacity
-  attr_accessor :song_list
+  attr_accessor :song_list, :guests_inside
 
-  def initialize(id, till, song_list, capacity)
+  def initialize(id, till, song_list, capacity, fee)
     @id = id
     @till = till
     @song_list = song_list
     @capacity = capacity
+    @guests_inside = []
+    @fee = fee
   end
 
-
+  def guest_taker(queue)
+    queue.each do |guest|
+      if guest.wallet>@fee
+        @guests_inside.push(guest)
+        guest.wallet -= @fee
+      end
+    end
+  end
 
 
 end
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -10,9 +10,9 @@ class RoomTest < Minitest::Test
     @guest2 = Guest.new("Paul", 50, "All My Loving")
     @guest3 = Guest.new("George", 30, "A Hard Day's Night")
     @guest4 = Guest.new("Ringo", 40, "With A Little Help From My Friends")
-    @guest4 = Guest.new("Pete", 10, "Twist and Shout")
-    @guest4 = Guest.new("James", 60, "The Unforgiven")
-    @guest4 = Guest.new("Bob", 3, "Blowin in the Wind")
+    @guest5 = Guest.new("Pete", 10, "Twist and Shout")
+    @guest5 = Guest.new("James", 60, "The Unforgiven")
+    @guest7 = Guest.new("Bob", 3, "Blowin in the Wind")
 
     @song1 = Song.new("Can't Buy Me Love", 2.11)
     @song2 = Song.new("A Hard Day's Night", 2.32)
@@ -20,14 +20,33 @@ class RoomTest < Minitest::Test
     @song4 = Song.new("With A Little Help From My Friends", 2.44)
     @song5 = Song.new("I Saw Her Standing There", 2.55)
     songs=[@song1,@song2,@song3,@song4,@song5]
-    @room1 = Room.new(1, 0, songs, 4)
+    @room1 = Room.new(1, 0, songs, 4, 5)
   end
-
 
   def test_room_song_list
     assert_equal(5, @room1.song_list.length)
   end
 
+  def test_check_guests_in__normal
+    queue0 = [@guest1, @guest2, @guest3, @guest4]
+    #Normal queue
+    @room1.guest_taker(queue0)
+    assert_equal(4, @room1.guests_inside.length)
+  end
+
+  def test_check_guests_in__extra_guest
+    skip
+    queue1 = [@guest1, @guest2, @guest3, @guest4, @guest5] #Extra guest
+
+    assert_equal()
+  end
+
+  def test_check_guests_in__poorman
+    skip
+    queue2 = [@guest1, @guest7, @guest3, @guest4]
+    #Poor man in the second place
+    assert_equal()
+  end
 
 end
 
