afadc90f0c31243c785b9d079e9e318450b47b18
Can Toraman <cantoraman@gmail.com>
2018-06-01 11:04:34 +0100

    working on guest setup

---
+++ guest.rb
@@ -0,0 +1,20 @@
+class Guest
+
+  attr_reader :name, :fav_song
+  attr_accessor :wallet
+
+  def initialize(name, wallet, fav_song)
+    @name = name
+    @wallet = wallet
+    @fav_song = fav_song
+  end
+
+
+
+
+
+
+
+
+
+end
